f05cf6d0f931ec0ba019372293237ad34a16eb7a
suzane <shuchika23aries@gmail.com>
2017-06-15 16:06:45 +0200

    Comment added

--- app/src/main/java/com/example/yudhisthira/localservice/MainActivity.java
+++ app/src/main/java/com/example/yudhisthira/localservice/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.yudhisthira.localservice;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView            _txtView;
